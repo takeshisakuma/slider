b59e1e4f6533b37be8570a12bc0a7e19958d4b8b
takeshisakuma <saude0518@gmail.com>
2021-04-20 02:53:29 +0900

    gap

--- README.md
+++ README.md
@@ -8,6 +8,9 @@
 
 5 秒ごとに自動送り
 
+IE11 非対応
+スマホ未対応
+
 ## DEMO
 
 https://takeshisakuma.github.io/slider/index.html
--- slider.js
+++ slider.js
@@ -7,27 +7,30 @@ const slider = function () {
   //Prevボタンの取得
   const prev = document.querySelector("#js-prev");
 
-  // slider(ul要素)の取得
+  //Stageの幅取得
+  const slideStage = document.querySelector("#js-slideStage");
+  const slideStageWidth = slideStage.clientWidth;
+
+  // slide(ul要素)の取得
   const slideGroup = document.querySelector("#js-slideGroup");
 
-  // slider(li要素一覧)の取得
-  const slideGroup_item = document.querySelectorAll(".js-slideItem");
+  // slide(li要素一覧)の取得
+  const slideItems = document.querySelectorAll(".js-slideItem");
 
   // liタグのwidthを取得
-  const sliderwidth = document.querySelector(".js-slideItem");
-  let width = sliderwidth.clientWidth;
+  const slideItemWidth = document.querySelector(".js-slideItem").clientWidth;
 
   //スライドの数を取得
-  const slideNumber = slideGroup_item.length;
+  const slideNumber = slideItems.length;
 
   //最初のスライドのクローン
-  const firstSlide1st = slideGroup_item[0].cloneNode(true);
+  const firstSlide1st = slideItems[0].cloneNode(true);
   //最初から2番目のスライドのクローン
-  const firstSlide2nd = slideGroup_item[1].cloneNode(true);
+  const firstSlide2nd = slideItems[1].cloneNode(true);
   //ラストスライドのクローン
-  const lastSlide1st = slideGroup_item[slideNumber - 1].cloneNode(true);
+  const lastSlide1st = slideItems[slideNumber - 1].cloneNode(true);
   //最後から二番目のスライドのクローン
-  const lastSlide2nd = slideGroup_item[slideNumber - 2].cloneNode(true);
+  const lastSlide2nd = slideItems[slideNumber - 2].cloneNode(true);
 
   //クローンをスライドの前後に挿入
   slideGroup.append(firstSlide1st);
@@ -47,9 +50,11 @@ const slider = function () {
   }, 1000);
 
   //スライド1枚目を中央にセット
-  //ずらす幅
-  const gap = 50;
-  slideGroup.style.transform = "translateX(" + (-width * counter + gap) + "px)";
+  //ずらす幅 (ステージ幅-スライド幅)/2
+  const gap = (slideStageWidth - slideItemWidth) / 2;
+
+  slideGroup.style.transform =
+    "translateX(" + (-slideItemWidth * counter + gap) + "px)";
 
   const goNext = () => {
     //ボタンを押した直後自動送りを10秒無効化
@@ -63,18 +68,18 @@ const slider = function () {
 
     //ulのスタイル変更(x方向に、-1スライドの幅XカウントPX)
     slideGroup.style.transform =
-      "translateX(" + (-width * counter + gap) + "px)";
+      "translateX(" + (-slideItemWidth * counter + gap) + "px)";
 
     //イベントリスナー追加(トランジション終了時)
     slideGroup.addEventListener("transitionend", () => {
       //カウントがスライド(ul)の数+初期値なら
-      if (counter == slideGroup_item.length + init) {
+      if (counter == slideItems.length + init) {
         //巻き戻しはトランジション無し
         slideGroup.style.transition = "0s";
 
         //巻き戻し実行
         slideGroup.style.transform =
-          "translateX(" + (-width * init + gap) + "px)";
+          "translateX(" + (-slideItemWidth * init + gap) + "px)";
 
         //カウントからスライド数分を減らす
         counter -= slideNumber;
@@ -95,9 +100,9 @@ const slider = function () {
     //カウントを1減らす
     counter--;
 
-    //ulのスタイル変更(x方向に、1スライドの幅XカウントPX) (""+ - widthにしないとだめ)
+    //ulのスタイル変更(x方向に、1スライドの幅XカウントPX)
     slideGroup.style.transform =
-      "translateX(" + (-width * counter + gap) + "px)";
+      "translateX(" + (-slideItemWidth * counter + gap) + "px)";
 
     //イベントリスナー追加(トランジション終了時)
     slideGroup.addEventListener("transitionend", () => {
@@ -107,7 +112,7 @@ const slider = function () {
         slideGroup.style.transition = "0s";
         //巻き戻し実行
         slideGroup.style.transform =
-          "translateX(" + (-width * (slideNumber + 1) + gap) + "px)";
+          "translateX(" + (-slideItemWidth * (slideNumber + 1) + gap) + "px)";
         //カウントに、スライド数を引く
         counter += slideNumber;
       }
